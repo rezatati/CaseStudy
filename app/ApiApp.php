0fd5ff347dd1850fec36271cc51543f7b9f2916d
rezatati <rezatati@gmail.com>
2021-08-23 17:42:42 +0430

    move composer to app dir and update gitignore

--- app/ApiApp.php
+++ app/ApiApp.php
@@ -3,7 +3,9 @@
 use Library\ErrorHandlers\DefaultErrorHandler;
 use Library\Routing\RoutServiceProvider;
 
+
 define("APP_ROOT", $_SERVER['DOCUMENT_ROOT'] . '/../');
+require APP_ROOT . "vendor/autoload.php";
 global $config;
 $config = include "config.php";
 class ApiApp
